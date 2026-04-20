f276096c12ace2c2f27d18fc7a8034d88546f4d7
Ronell1243 <mesogem1243@gmail.com>
2026-04-21 03:36:49 +0900

    [Feat] 도움말 창 추가

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -6,10 +6,21 @@ using System.Collections.Generic;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject HelpPanel;
+
     public void GameStartButtonAction()
     {
         //본인 첫 씬 이름 쓰기
         SceneManager.LoadScene("Level_1");
     }
 
+    public void OpenHelpPanel()
+    {
+        HelpPanel.SetActive(true);
+    }
+
+    public void CloseHelpPanel()
+    {
+        HelpPanel.SetActive(false);
+    }
 }
